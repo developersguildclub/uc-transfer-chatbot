3e2aa16318a553dd2059ba911c38ba1600c40ef8
Ravi Purnama <ravihilmifuad@gmail.com>
2026-05-14 17:44:07 -0700

    Added app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,9 @@
+from flask import Flask, redirect, render_template, request, session
+from cs50 import SQL
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
